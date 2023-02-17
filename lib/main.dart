6532c3d9293d0f303e6f4ccf8e97236c12bf098b
Admin <makwanadharmishtha44@gmail.com>
2023-02-17 21:26:31 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:font_image_1/Screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    routes: {
+      '/': (context) => Project5A(),
+    },
+  ));
+}
